da2e9ad1fe11f5f2fcb0dc671746306677216835
jinwoo1990 <jinwoohong77@gmail.com>
2021-03-10 18:55:47 +0900

    Generate App structure and create users app

---
+++ users/admin.py
@@ -0,0 +1,33 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from . import models
+
+
+# UserAdmin 으로 extension 하면 user 는 충분히 잘 작성된 것을 사용할 수 있음
+@admin.register(models.User)
+class CustomUserAdmin(UserAdmin):
+
+    """ Custom User Admin """
+
+    # # admin 창에서 나열한 정보를 표처럼 리스트업
+    # list_display = ("username", "gender", "email", "language", "currency", "superhost")
+    #
+    # # admin 창에서 나열한 정보로 필터 가능하게
+    # list_filter = ("language", "currency", "superhost",)
+
+    fieldsets = UserAdmin.fieldsets + (
+        (
+            "Custom Profile",
+            {
+                "fields": (
+                    "avatar",
+                    "gender",
+                    "bio",
+                    "birthdate",
+                    "language",
+                    "currency",
+                    "superhost"
+                )
+            },
+        ),
+    )
