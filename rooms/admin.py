02a0a095a019185d397543855a8b4f0675f700e3
jinwoo1990 <jinwoohong77@gmail.com>
2021-03-12 11:07:00 +0900

    rooms admin 설정

--- rooms/admin.py
+++ rooms/admin.py
@@ -7,6 +7,14 @@ class ItemAdmin(admin.ModelAdmin):
 
     """ Item Admin Definition """
 
+    list_display = (
+        "name",
+        "used_by",
+    )
+
+    def used_by(self, obj):
+        return obj.rooms.count()
+
     pass
 
 
@@ -15,7 +23,82 @@ class RoomAdmin(admin.ModelAdmin):
 
     """ Room Admin Definition """
 
-    pass
+    # section 나누기
+    fieldsets = (
+        (
+            "Basic Info",
+            {"fields": ("name", "description", "country", "address", "price")}
+        ),
+        (
+            "Times",
+            {"fields": ("check_in", "check_out", "instant_book")}
+        ),
+        (
+            "Spaces",
+            {"fields": ("guests", "beds", "bedrooms", "baths")}
+        ),
+        (
+            "More About the Space",
+            {
+                "classes": ("collapse",),  # hide 할 수 있는 섹션으로 만들기
+                "fields": ("amenities", "facilities", "house_rules")
+            }
+        ),
+        (
+            "Last Details",
+            {"fields": ("host",)}
+        ),
+    )
+
+    list_display = (
+        "name",
+        "country",
+        "city",
+        "price",
+        "address",
+        "guests",
+        "beds",
+        "bedrooms",
+        "baths",
+        "check_in",
+        "check_out",
+        "instant_book",
+        "count_amenities",
+        "count_photos",
+    )
+
+    ordering = ("name", "price", "bedrooms")
+
+    list_filter = (
+        "instant_book",
+        "host__superhost",
+        "room_type",
+        "city",
+        "amenities",
+        "facilities",
+        "house_rules",
+        "country",
+    )
+
+    # search option 으로 ^ 을 주면 startswith 로 검색, 아니면 icontatins 인데 django 문서에 나와 있음
+    search_fields = ("=city", "^host__username")  # 복수 필드로 검색
+
+    filter_horizontal = (
+        "amenities",
+        "facilities",
+        "house_rules",
+    )
+
+    # self 는 클래스 자신, obj 는 row
+    def count_amenities(self, obj):
+        # print(obj.amenities.count())
+        return obj.amenities.count()
+
+    # 표시되는 컬럼명을 바꿀 수 있음
+    # count_amenities.short_description = "hello sexy!"
+
+    def count_photos(self, obj):
+        return obj.photos.count()
 
 
 @admin.register(models.Photo)
--- rooms/models.py
+++ rooms/models.py
@@ -66,7 +66,7 @@ class Photo(core_models.TimeStampedModel):
     # 아래 코드는 class 위치 때문에 에러
     # room = models.ForeignKey(Room, on_delete=models.CASCADE)
     # django 는 아래와 같이 "Room" 으로 작성하면 코드의 아래쪽에 있더라도 불러올 수 있음
-    room = models.ForeignKey("Room", on_delete=models.CASCADE)
+    room = models.ForeignKey("Room", related_name="photos", on_delete=models.CASCADE)
 
     def __str__(self):
         return self.caption
@@ -91,12 +91,13 @@ class Room(core_models.TimeStampedModel):
     instant_book = models.BooleanField(default=False)
     # 다른 모델 참조 (room 에서 연결이 시작됨, many to one relationship, many rooms to one user, room 은 하나의 유저만 가짐)
     # Foreign key 로 user 의 id 를 가르키고 이것으로 user 의 정보를 참조
-    host = models.ForeignKey("users.User", on_delete=models.CASCADE)
-    room_type = models.ForeignKey(RoomType, on_delete=models.SET_NULL, null=True)
+    # related_name 은 _set 으로 기본으로 붙는 attribute을 바꿔줌 (room_set -> rooms)
+    host = models.ForeignKey("users.User", related_name="rooms", on_delete=models.CASCADE)
+    room_type = models.ForeignKey("RoomType", related_name="rooms", on_delete=models.SET_NULL, null=True)
     # many to many (여러가지 room_type 을 가질 수 있음)
-    amenities = models.ManyToManyField("Amenity", blank=True)
-    facilities = models.ManyToManyField("Facility", blank=True)
-    house_rules = models.ManyToManyField("HouseRule", blank=True)
+    amenities = models.ManyToManyField("Amenity", related_name="rooms", blank=True)
+    facilities = models.ManyToManyField("Facility", related_name="rooms", blank=True)
+    house_rules = models.ManyToManyField("HouseRule", related_name="rooms", blank=True)
 
     def __str__(self):
         return self.name
