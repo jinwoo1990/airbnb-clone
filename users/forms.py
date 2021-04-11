34650c6271195e590f1e77d88e04d75d4612a046
jinwoo1990 <jinwoohong77@gmail.com>
2021-04-11 12:33:58 +0900

    signup 기능 개발

--- users/forms.py
+++ users/forms.py
@@ -41,3 +41,42 @@ class LoginForm(forms.Form):
                 self.add_error("password", forms.ValidationError("Password is wrong"))
         except models.User.DoesNotExist:
             self.add_error("email", forms.ValidationError("User does not exist"))
+
+
+class SignUpForm(forms.Form):
+    first_name = forms.CharField(max_length=80)
+    last_name = forms.CharField(max_length=80)
+    email = forms.EmailField()
+    password = forms.CharField(widget=forms.PasswordInput)
+    password1 = forms.CharField(widget=forms.PasswordInput, label="Confirm Password")
+
+    def clean_email(self):
+        email = self.cleaned_data.get("email")
+        try:
+            models.User.objects.get(email=email)
+            raise forms.ValidationError("User already exists with that email")
+        except models.User.DoesNotExist:
+            return email
+
+    # 선언된 순서대로 clean 되어 cleaned_data 에 담기게 되는데 password1 은 password 로 선언된 함수에서는 아직 정의되지 않아 더 밑에 있는
+    # password1 기준 함수로 작성해야 함
+    def clean_password1(self):
+        password = self.cleaned_data.get("password")
+        password1 = self.cleaned_data.get("password1")
+
+        if password != password1:
+            raise forms.ValidationError("Password confirmation does not match")
+        else:
+            return password
+
+    def save(self):
+        first_name = self.cleaned_data.get("first_name")
+        last_name = self.cleaned_data.get("last_name")
+        email = self.cleaned_data.get("email")
+        password = self.cleaned_data.get("password")
+
+        user = models.User.objects.create_user(email, email=email, password=password)
+
+        user.first_name = first_name
+        user.last_name = last_name
+        user.save()
